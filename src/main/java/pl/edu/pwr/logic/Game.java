6d0ef4f2726bbc6a0769478a4cfe8a9da39f0828
Jakub Kobus <jkb.kobus@gmail.com>
2025-12-17 11:52:36 +0100

    fix: rules for iteration 1

--- src/main/java/pl/edu/pwr/logic/Game.java
+++ src/main/java/pl/edu/pwr/logic/Game.java
@@ -1,9 +1,6 @@
 package pl.edu.pwr.logic;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.ArrayList;
 
 public class Game {
   private final Board board;
@@ -34,65 +31,31 @@ public class Game {
 
     for(Board.Point n : neighbors) {
       Stone neighborStone = board.get(n.x(), n.y());
+      
       if(neighborStone == opponentColor)
-        if(countGroupLiberties(n.x(), n.y(), opponentColor) == 0)
-          removeGroup(n.x(), n.y(), opponentColor);
+        if(countSingleStoneLiberties(n.x(), n.y()) == 0)
+          captureSingleStone(n.x(), n.y());
     }
   }
 
-  private int countGroupLiberties(int startX, int startY, Stone color) {
-    Set<String> visited = new HashSet<>();
-    Set<String> liberties = new HashSet<>();
-    List<Board.Point> stack = new ArrayList<>();
-    stack.add(new Board.Point(startX, startY));
+  private int countSingleStoneLiberties(int x, int y) {
+    int liberties = 0;
+    List<Board.Point> neighbors = board.getNeighbors(x, y);
+    
+    for(Board.Point n : neighbors)
+      if(board.isEmpty(n.x(), n.y()))
+        liberties++;
 
-    while(!stack.isEmpty()) {
-      Board.Point current = stack.remove(0);
-      String key = current.x() + "," + current.y();
-
-      if(visited.contains(key))
-        continue;
-      visited.add(key);
-
-      for(Board.Point neighbor : board.getNeighbors(current.x(), current.y())) {
-        Stone s = board.get(neighbor.x(), neighbor.y());
-        if(s == Stone.NONE)
-          liberties.add(neighbor.x() + "," + neighbor.y());
-        else if(s == color)
-          stack.add(neighbor);
-      }
-    }
-    return liberties.size();
+    return liberties;
   }
 
-  private void removeGroup(int startX, int startY, Stone color) {
-    List<Board.Point> stack = new ArrayList<>();
-    stack.add(new Board.Point(startX, startY));
-    Set<String> visited = new HashSet<>();
+  private void captureSingleStone(int x, int y) {
+    board.placeStone(x, y, Stone.NONE);
     
-    int stonesCaptured = 0;
-
-    while(!stack.isEmpty()) {
-      Board.Point current = stack.remove(0);
-      String key = current.x() + "," + current.y();
-
-      if(visited.contains(key))
-        continue;
-      visited.add(key);
-
-      board.placeStone(current.x(), current.y(), Stone.NONE);
-      stonesCaptured++;
-
-      for(Board.Point neighbor : board.getNeighbors(current.x(), current.y()))
-        if(board.get(neighbor.x(), neighbor.y()) == color)
-          stack.add(neighbor);
-    }
-    
-    if(currentPlayer == Stone.BLACK) {
-        blackPrisoners += stonesCaptured;
-    } else {
-        whitePrisoners += stonesCaptured;
-    }
+    if(currentPlayer == Stone.BLACK)
+      blackPrisoners++;
+    else
+      whitePrisoners++;
   }
 
   public Board getBoard() {
