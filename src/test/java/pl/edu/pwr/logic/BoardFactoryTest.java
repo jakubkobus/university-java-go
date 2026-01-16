b517a4b30b49f3eb73fe68061f348c2bb34ce372
Jakub Kobus <jkb.kobus@gmail.com>
2026-01-16 17:02:04 +0100

    feat(test): add javadoc

--- src/test/java/pl/edu/pwr/logic/BoardFactoryTest.java
+++ src/test/java/pl/edu/pwr/logic/BoardFactoryTest.java
@@ -3,8 +3,24 @@ package pl.edu.pwr.logic;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Testy dla klasy BoardFactory.
+ * 
+ * Testuje funkcjonalność tworzenia plansz o różnych rozmiarach
+ * i walidację błędnych rozmiarów.
+ * 
+ * @author Jakub Kobus, Dawid Leśkiewicz
+ * @version 1.0
+ * @see BoardFactory
+ */
 class BoardFactoryTest {
 
+  /**
+   * Test tworzenia plansz o prawidłowych rozmiarach.
+   * 
+   * Sprawdza czy fabryka tworzy plansze o rozmiarach 19x19, 13x13 i 9x9
+   * bez rzucania wyjątków.
+   */
   @Test
   void testCreateValidBoard() {
     assertDoesNotThrow(() -> BoardFactory.createBoard(19));
@@ -12,6 +28,12 @@ class BoardFactoryTest {
     assertDoesNotThrow(() -> BoardFactory.createBoard(9));
   }
 
+  /**
+   * Test tworzenia planszy o nieprawidłowym rozmiarze.
+   * 
+   * Sprawdza czy fabryka rzuca IllegalArgumentException
+   * przy próbie utworzenia planszy o niedozwolonym rozmiarze (np. 10).
+   */
   @Test
   void testCreateInvalidBoardThrowsException() {
     Exception exception = assertThrows(IllegalArgumentException.class, () -> {
--- src/test/java/pl/edu/pwr/logic/GameStateTest.java
+++ src/test/java/pl/edu/pwr/logic/GameStateTest.java
@@ -4,6 +4,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Testy dla stanów gry (game states) i przejść między fazami.
+ * 
+ * Testuje:
+ * <ul>
+ *   <li>Stan IN_PROGRESS (gra aktywna)</li>
+ *   <li>Stan CLEANUP (faza sprzątania terytoriów)</li>
+ *   <li>Stan FINISHED (gra skończona)</li>
+ *   <li>Przejścia między stanami na podstawie pasów</li>
+ *   <li>Usuwanie martwych kamieni w fazie CLEANUP</li>
+ *   <li>Umieszczanie jeńców na terenie przeciwnika w fazie CLEANUP</li>
+ * </ul>
+ * 
+ * @author Jakub Kobus, Dawid Leśkiewicz
+ * @version 1.0
+ * @see Game
+ * @see GameState
+ */
 class GameStateTest {
   private Game game;
 
@@ -12,6 +30,17 @@ class GameStateTest {
     game = new Game(9);
   }
 
+  /**
+   * Test przepływu pasów i zmian stanów gry.
+   * 
+   * Sprawdza:
+   * <ol>
+   *   <li>Gra zaczyna się w stanie IN_PROGRESS</li>
+   *   <li>Jeden pas zmienia turę, ale pozostawia grę w IN_PROGRESS</li>
+   *   <li>Ruch resetuje licznik pasów</li>
+   *   <li>Dwa pasy z rzędu przechodzą grę do stanu CLEANUP</li>
+   * </ol>
+   */
   @Test
   void testPassFlow() {
     assertEquals(GameState.IN_PROGRESS, game.getState());
@@ -29,6 +58,17 @@ class GameStateTest {
     assertEquals(GameState.CLEANUP, game.getState(), "Dwa pasy z rzędu powinny uruchomić CLEANUP");
   }
 
+  /**
+   * Test walidacji usuwania martwych kamieni.
+   * 
+   * Sprawdza:
+   * <ol>
+   *   <li>Tylko martwe kamienie przeciwnika mogą być usunięte</li>
+   *   <li>Gracz nie może usuwać swoich własnych kamieni</li>
+   *   <li>Usunięty kamień zostaje dodany do licznika jeńców</li>
+   *   <li>Nie można usuwać pól poza granicami lub już pótych</li>
+   * </ol>
+   */
   @Test
   void testRemoveDeadStoneValidation() {
     game.pass();
@@ -52,6 +92,13 @@ class GameStateTest {
     assertTrue(result.contains("ERR"));
   }
 
+  /**
+   * Test umieszczania jeńców na terenie przeciwnika.
+   * 
+   * Sprawdza czy gracz może umieszczać jeńców na wolnych polach
+   * w fazie CLEANUP do zaznaczania swoich terytoriów,
+   * i czy liczba jeńców zmniejsza się po umieszczeniu.
+   */
   @Test
   void testFillPrisoner() {
     Game g = new Game(9);
